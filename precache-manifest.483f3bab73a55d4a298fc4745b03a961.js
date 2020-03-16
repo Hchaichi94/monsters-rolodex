e589c6dbf63e77ee5c1fd3404f3d59550bf33f43
akrem <46078485+Hchaichi94@users.noreply.github.com>
2020-03-16 18:19:21 +0100

    Updates

---
+++ precache-manifest.483f3bab73a55d4a298fc4745b03a961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac0da69eea0d1ddc3e5afe3884859a7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2f03269dd907b2cef8b",
+    "url": "/monsters-rolodex/static/css/main.3402e5a6.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f03269dd907b2cef8b",
+    "url": "/monsters-rolodex/static/js/main.90352cd1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
